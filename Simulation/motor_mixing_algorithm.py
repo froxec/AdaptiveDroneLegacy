f31df56e0c2f0ce4de28484c17d54baee217504a
froxec <picmego@gmail.com>
2022-07-09 19:25:24 +0200

    motor_mixing_algorithm

---
+++ Simulation/motor_mixing_algorithm.py
@@ -0,0 +1,16 @@
+#X-configuration
+#ccw is positive
+#       4  🢁🢁🢁  2
+#         \    /  
+#          \  /
+#           \/
+#           /\
+#          /  \
+#         /    \
+#       3        1
+
+
+motor1 = thrust + roll - pitch - yaw
+motor2 = thrust + roll + pitch + yaw
+motor3 = thrust - roll - pitch + yaw
+motor4 = thrust - roll + pitch - yaw
