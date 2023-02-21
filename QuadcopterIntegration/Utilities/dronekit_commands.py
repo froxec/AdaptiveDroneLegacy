ef3488455ad208b83b4cc1f5ff3b1fc326c7d938
czuczo <picmego@gmail.com>
2023-02-21 12:44:21 +0000

    state read commands

--- QuadcopterIntegration/Utilities/dronekit_commands.py
+++ QuadcopterIntegration/Utilities/dronekit_commands.py
@@ -5,3 +5,9 @@ def set_attitude(vehicle, roll, pitch, yaw, thrust):
     quaternion = euler_to_quaternion(roll, pitch, yaw)
     msg = vehicle.send_mavlink(mavutil.mavlink.MAVLink_set_attitude_target_message(1, 0, 0, 7, quaternion, 0, 0, 0, thrust))
     
+def get_state(vehicle):
+    position = vehicle.location.local_frame
+    velocity = vehicle.velocity
+    state = [position.north, position.east, -position.down]
+    state.extend(velocity)
+    return state
