5481cb35ad795a0845297b7381ac3a1e7e0c6eea
czuczo <picmego@gmail.com>
2023-05-05 18:46:00 +0200

    pi_position_controller.py

--- Factories/ControllersFactory/position_controllers/position_controller_parameters.py
+++ Factories/ControllersFactory/position_controllers/position_controller_parameters.py
@@ -1,3 +1,5 @@
+ANGULAR_VELOCITY_RANGE = [0, 800]
+PWM_RANGE = [1120, 1920]
 
 thrust_compensator_parameters = {
     'Kp': 0.0,
---
+++ QuadcopterIntegration/HIL/pi_position_controller.py
@@ -0,0 +1,31 @@
+from Factories.ControllersFactory.position_controllers.mpc import ModelPredictiveControl
+from Factories.ModelsFactory.linear_models import AugmentedLinearizedQuadNoYaw
+from Factories.ModelsFactory.model_parameters import Z550_parameters
+from Factories.ToolsFactory.Converters import MPC_input_converter, MPC_output_converter
+from Factories.CommunicationFactory.interfaces import ControllerInterface
+from Factories.SimulationsFactory.TrajectoriesDepartment.trajectories import SinglePoint
+from Factories.CommunicationFactory.communication_parameters import *
+from Factories.ControllersFactory.position_controllers.position_controller import PositionController
+from Factories.CommunicationFactory.interfaces import ControllerInterface
+from Factories.ControllersFactory.position_controllers.position_controller_parameters import *
+
+import numpy as np
+OUTER_LOOP_FREQ = 10
+trajectory = SinglePoint([0, 0, 10])
+parameters = Z550_parameters
+if __name__ == "__main__":
+    print("Initialisation started...")
+    prediction_model = AugmentedLinearizedQuadNoYaw(parameters, Ts=1/OUTER_LOOP_FREQ)
+    mpc = ModelPredictiveControl(prediction_model, OUTER_LOOP_FREQ, pred_horizon=10)
+    input_converter = MPC_input_converter(np.zeros(6), np.array([parameters['m']*parameters['g'], 0, 0]))
+    output_converter = MPC_output_converter( np.array([parameters['m']*parameters['g'], 0, 0]), parameters['Kt'], ANGULAR_VELOCITY_RANGE)
+    interface = ControllerInterface(RASP_IP, RASP_PORT, PC_IP, PC_PORT)
+    position_controller = PositionController(mpc,
+                                             input_converter,
+                                             output_converter,
+                                             trajectory, 
+                                             interface)
+    print("Initialisation finished.")
+
+    while True:
+        position_controller()
