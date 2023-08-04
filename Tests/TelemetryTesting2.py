93da8456d8c1cc3dcddf9f65e76cbcf7b919a631
czuczo_pi <picmego@gmail.com>
2023-08-04 21:46:17 +0200

    telem updated

--- Tests/TelemetryTesting2.py
+++ Tests/TelemetryTesting2.py
@@ -6,19 +6,20 @@ import time
 if __name__ == "__main__":
     transport = SerialTransport()
     tlm = Pytelemetry(transport)
-    transport.connect({'port':'/dev/pts/4', 'baudrate':9600})
-    tlm.publish(chr(2), 0.9, 'float32')
+    transport.connect({'port':'/dev/ttyS0', 'baudrate':115200})
 
+    tlm.publish(chr(10), 1, 'int8')
+    
     def printer(topic, data, opts):
         print(topic, " : ", data)
 
     tlm.subscribe(None, printer)
 
     timeout = time.time() + 20
+    start = time.time()
     while True:
         tlm.update()
-        if time.time() > timeout:
-            break
+        time.sleep(0.05)
 
 
     transport.disconnect()
