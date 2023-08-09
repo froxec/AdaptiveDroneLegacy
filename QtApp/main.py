814b8a61f3620bc0fb2b2105bd154cae8826130b
czuczo <picmego@gmail.com>
2023-08-09 20:02:27 +0200

    praca update

--- QtApp/main.py
+++ QtApp/main.py
@@ -315,7 +315,7 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-    tm = TelemetryManagerThreadGCS(serialport='/dev/pts/5',
+    tm = TelemetryManagerThreadGCS(serialport='/dev/pts/7',
                                    baudrate=115200,
                                    update_freq=10)
     app = QApplication(sys.argv)
