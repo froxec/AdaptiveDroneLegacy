e166e82196ecdebae5c2c639676347a1c41b91c6
czuczo <picmego@gmail.com>
2023-09-04 15:32:00 +0200

    parameters_update

--- Factories/ControllersFactory/position_controllers/mpc_parameters.py
+++ Factories/ControllersFactory/position_controllers/mpc_parameters.py
@@ -9,7 +9,7 @@
 # new parameters for sparse mpc
 MPC_PARAMETERS_MAPPING = {
     "LINEARIZED": {'Q_base': [0.5, 0.5, 0.5, 1, 1, 1], 'P_base': [1, 100, 100]},
-    "LINEARIZED_NORMALIZED": {'Q_base': [40, 40, 100, 1, 1, 1], 'P_base': [1, 1, 1]},
+    "LINEARIZED_NORMALIZED": {'Q_base': [160, 160, 160, 1, 1, 1], 'P_base': [1, 1, 1]},
     "TRANSLATIONAL_DYNAMICS": {'Q_base': [5, 5, 5, 1, 1, 1], 'P_base': [1, 1, 1]},
     "TRANSLATIONAL_DYNAMICS_NORMALIZED": {'Q_base': [1, 1, 1, 0.01, 0.01, 0.01], 'P_base':[1, 1, 0.01]}
 }
--- Multiprocessing/PARAMS.py
+++ Multiprocessing/PARAMS.py
@@ -1,6 +1,6 @@
 from Factories.ConfigurationsFactory.modes import MPCModes
 from Factories.SimulationsFactory.TrajectoriesDepartment.trajectories import SinglePoint
-from Factories.ModelsFactory.model_parameters import Z550_parameters, Iris_parameters
+from Factories.ModelsFactory.model_parameters import Z550_parameters_new, Iris_parameters
 import numpy as np
 
 # OPC SERVER
@@ -21,18 +21,18 @@ NORMALIZE = True
 ANGULAR_VELOCITY_RANGE = [0, 800]
 PWM_RANGE = [1120, 1920]
 MASS_PERTURBATION = 0.0
-PREDICTOR_PARAMETERS = Iris_parameters
+PREDICTOR_PARAMETERS = Z550_parameters_new
 PREDICTOR_PARAMETERS['m'] = PREDICTOR_PARAMETERS['m'] + MASS_PERTURBATION
 
 # MPC PARAMES
-HORIZON = 10
+HORIZON = 20
 MPC_MODE = MPCModes.CONSTRAINED
 TRAJECTORY = SinglePoint([0, 0, 3])
 THROTTLE_MIN = 0.1
 THRUST_MIN, THRUST_MAX = THROTTLE_MIN * PREDICTOR_PARAMETERS['throttle_thrust_slope'] + PREDICTOR_PARAMETERS['throttle_thrust_intercept'],\
                         PREDICTOR_PARAMETERS['throttle_max'] * PREDICTOR_PARAMETERS['throttle_thrust_slope'] + PREDICTOR_PARAMETERS['throttle_thrust_intercept']
-MPC_CONSTRAINTS = {"x_bounds": {'lower': np.array([-100000, -100000, -100000, -5, -5, -5]),
-                                'upper': np.array([100000, 100000, 100000, 5, 5, 5])},
+MPC_CONSTRAINTS = {"x_bounds": {'lower': np.array([-100000, -100000, -100000, -7, -7, -7]),
+                                'upper': np.array([100000, 100000, 100000, 7, 7, 7])},
                    "u_bounds": {'lower': np.array([THRUST_MIN, -np.pi/6, -np.pi/6]),
                                 'upper': np.array([THRUST_MAX, np.pi/6, np.pi/6])},
                    "delta_x_bounds": {'lower': np.array([-1000, -1000, -1000, -1000, -1000, -1000]),
@@ -41,8 +41,8 @@ MPC_CONSTRAINTS = {"x_bounds": {'lower': np.array([-100000, -100000, -100000, -5
                                    'upper': np.array([3, np.pi/12,np.pi/12])}}
 
 # ADAPTIVE_PARAMETERS:
-As = np.diag([-15, -15, -0.1])
-BANDWIDTHS = [0.5, 0.2, 0.2]
+As = np.diag([-15, -15, -1])
+BANDWIDTHS = [0.2, 0.2, 0.2]
 
 # ESTIMATOR PARAMETERS
 SAMPLING_FREQ = 20
