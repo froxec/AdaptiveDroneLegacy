35b188d90b3dd3c34efdc408210e4ce6adee9c1a
czuczo <picmego@gmail.com>
2023-09-06 20:24:56 +0200

    virtual serial over udp updated

--- Factories/CommunicationFactory/Telemetry/python-socket2serial.py
+++ Factories/CommunicationFactory/Telemetry/python-socket2serial.py
@@ -8,22 +8,29 @@ import select
 import argparse
 
 parser = argparse.ArgumentParser(description='Creates a virtual pty for a remote tcp/udp socket')
-parser.add_argument('host', type=str)
-parser.add_argument('port', type=int)
+parser.add_argument('rhost', type=str)
+parser.add_argument('rport', type=int)
+parser.add_argument('lhost', type=str)
+parser.add_argument('rport', type=int)
 parser.add_argument('-u', '--udp', action='store_true')
 args = parser.parse_args()
 
-lhost = args.host
-lport = args.port
+rhost = args.rhost
+rport = args.rport
+lhost = args.lhost
+lport = args.lport
+
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM if args.udp else socket.SOCK_STREAM)
-    s.connect((lhost, lport))
+    s_local = socket.socket(socket.AF_INET, socket.SOCK_DGRAM if args.udp else socket.SOCK_STREAM)
+    s_local.connect((lhost, lport))
+    s_remote = socket.socket(socket.AF_INET, socket.SOCK_DGRAM if args.udp else socket.SOCK_STREAM)
+    s_remote.connect((rhost, rport))
     master, slave = pty.openpty()
 #    tty.setraw(master, termios.TCSANOW)
-    print('PTY: Opened {} for {}:{}'.format(os.ttyname(slave), lhost, lport))
+    print('PTY: Opened {} for {}:{}'.format(os.ttyname(slave), rhost, rport))
     mypoll = select.poll()
-    mypoll.register(s, select.POLLIN)
+    mypoll.register(s_local, select.POLLIN)
     mypoll.register(master, select.POLLIN)
 
     try:
@@ -32,10 +39,11 @@ def main():
             for fd,event in fdlist:
                 data = os.read(fd, 4096)
                 print(data)
-                write_fd = s.fileno() if fd == master else master
+                write_fd = s_remote.fileno() if fd == master else master
                 os.write(write_fd, data)
     finally:
-        s.close()
+        s_local.close()
+        s_remote.close()
         os.close(master)
         os.close(slave)
     
