e0b5ba58dd872f81bb296a49cfb0e23f9e9391be
czuczo <picmego@gmail.com>
2023-09-21 14:35:39 +0200

    adaptation working really fast

--- Multiprocessing/PARAMS.py
+++ Multiprocessing/PARAMS.py
@@ -21,7 +21,7 @@ NORMALIZE = True
 ANGULAR_VELOCITY_RANGE = [0, 800]
 PWM_RANGE = [1120, 1920]
 MASS_PERTURBATION = 0.0
-PREDICTOR_PARAMETERS = Z550_parameters_new
+PREDICTOR_PARAMETERS = Iris_parameters
 PREDICTOR_PARAMETERS['m'] = PREDICTOR_PARAMETERS['m'] + MASS_PERTURBATION
 
 # MPC PARAMES
@@ -31,18 +31,18 @@ TRAJECTORY = SinglePoint([0, 0, 3])
 THROTTLE_MIN = 0.1
 THRUST_MIN, THRUST_MAX = THROTTLE_MIN * PREDICTOR_PARAMETERS['throttle_thrust_slope'] + PREDICTOR_PARAMETERS['throttle_thrust_intercept'],\
                         PREDICTOR_PARAMETERS['throttle_max'] * PREDICTOR_PARAMETERS['throttle_thrust_slope'] + PREDICTOR_PARAMETERS['throttle_thrust_intercept']
-MPC_CONSTRAINTS = {"x_bounds": {'lower': np.array([-100000, -100000, -100000, -10, -10, -10]),
-                                'upper': np.array([100000, 100000, 100000, 10, 10, 10])},
+MPC_CONSTRAINTS = {"x_bounds": {'lower': np.array([-100000, -100000, -100000, -5, -5, -5]),
+                                'upper': np.array([100000, 100000, 100000, 5, 5, 5])},
                    "u_bounds": {'lower': np.array([THRUST_MIN, -np.pi/6, -np.pi/6]),
                                 'upper': np.array([THRUST_MAX, np.pi/6, np.pi/6])},
                    "delta_x_bounds": {'lower': np.array([-1000, -1000, -1000, -1000, -1000, -1000]),
                                    'upper': np.array([1000, 1000, 1000, 1000, 1000, 1000])},
-                   "delta_u_bounds": {'lower': np.array([-8, -np.pi/24, -np.pi/24]),
-                                   'upper': np.array([8, np.pi/24,np.pi/24])}}
+                   "delta_u_bounds": {'lower': np.array([-100, -np.pi/24, -np.pi/24]),
+                                   'upper': np.array([100, np.pi/24,np.pi/24])}}
 
 # ADAPTIVE_PARAMETERS:
-As = np.diag([-5, -5, -0.1])
-BANDWIDTHS = [0.1, 0.1, 0.1]
+As = np.diag([-5, -5, -5])
+BANDWIDTHS = [5, 0.5, 0.5]
 L1_INPUT_BANDWIDTHS = [5, 2, 2]
 # ESTIMATOR PARAMETERS
 SAMPLING_FREQ = 20
