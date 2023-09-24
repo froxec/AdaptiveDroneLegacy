e5d6de995602cbb249cd62882e4c7b3659f3e896
czuczo_pi <picmego@gmail.com>
2023-09-24 23:19:56 +0200

    working sim params

--- Multiprocessing/PARAMS.py
+++ Multiprocessing/PARAMS.py
@@ -1,6 +1,5 @@
 
 from Factories.ConfigurationsFactory.modes import MPCModes
-from Factories.SimulationsFactory.TrajectoriesDepartment.trajectories import SinglePoint
 from Factories.ModelsFactory.model_parameters import Z550_parameters_new, Iris_parameters
 import numpy as np
 
@@ -17,18 +16,21 @@ REDIS_HOST = "127.0.0.1"
 REDIS_PORT = 6379
 DB_NUM = 0
 
+# SQUARE TRAJECTORY
+SQUARE_TRAJECTORY_HEIGHT = 5
+
 # MODEL
 NORMALIZE = True
 ANGULAR_VELOCITY_RANGE = [0, 800]
 PWM_RANGE = [1120, 1920]
 MASS_PERTURBATION = 0.0
-PREDICTOR_PARAMETERS = Z550_parameters_new
+PREDICTOR_PARAMETERS = Iris_parameters
 PREDICTOR_PARAMETERS['m'] = PREDICTOR_PARAMETERS['m'] + MASS_PERTURBATION
 
 # MPC PARAMES
 HORIZON = 10
 MPC_MODE = MPCModes.CONSTRAINED
-TRAJECTORY = SinglePoint([0, 0, 3])
+TRAJECTORY = [0, 0, 3]
 THROTTLE_MIN = 0.1
 THRUST_MIN, THRUST_MAX = THROTTLE_MIN * PREDICTOR_PARAMETERS['throttle_thrust_slope'] + PREDICTOR_PARAMETERS['throttle_thrust_intercept'],\
                         PREDICTOR_PARAMETERS['throttle_max'] * PREDICTOR_PARAMETERS['throttle_thrust_slope'] + PREDICTOR_PARAMETERS['throttle_thrust_intercept']
@@ -42,7 +44,7 @@ MPC_CONSTRAINTS = {"x_bounds": {'lower': np.array([-100000, -100000, -100000, -5
                                    'upper': np.array([100, np.pi/24, np.pi/24])}}
 
 # ADAPTIVE_PARAMETERS:
-As = np.diag([-15, -15, -1])
+As = np.diag([-15, -15, -0.1])
 BANDWIDTHS = [2.5, 0.5, 0.5]
 L1_INPUT_BANDWIDTHS = [5, 2, 2]
 # ESTIMATOR PARAMETERS
